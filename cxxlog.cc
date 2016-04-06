855e0fb7631675a9b10797c108555f74c2d73799
HAO Hongting <haohongting@live.com>
2016-04-06 14:42:07 +0800

    code initial commit.

---
+++ cxxlog.cc
@@ -0,0 +1,172 @@
+#include <stdarg.h>
+#include <string.h>
+#include <sys/time.h>
+#include "cxxlog.h"
+
+namespace cxxlog {
+
+Logger *Logger::instance_ = NULL;
+pthread_once_t Logger::pOnce_ = PTHREAD_ONCE_INIT;
+thread_local pid_t myThreadId_ = 0;
+
+
+Logger::Logger()
+    :logPathName_(""),
+    logFp_(NULL),
+    rotateSize_(1024*1024*10),
+    currentSize_(0),
+    level_(LogLevel::TRACE),
+    stdOut_(false),
+    lastFlushSecs_(0),
+    forceFlushIntervel_(5) 
+{
+    // default rotate size is 10MB.
+    // default forceFlushIntervel is 5 seconds.
+}
+
+Logger::~Logger()
+{
+    Close();
+}
+
+void Logger::Open(const char *pathName)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    if (logFp_) {
+        fprintf(stderr, "cxxlog, Logger is already opened as %s\n", logPathName_.c_str());
+        return;
+    }
+
+    logPathName_.assign(pathName);
+
+    char pid[32];
+    snprintf(pid, 32, ".%d", (int)::getpid());
+    logPathName_.append(pid);
+
+    logFp_ = ::fopen(logPathName_.c_str(), "a+");
+    if (!logFp_) {
+        perror("Logger open failed");
+        return;
+    }
+}
+
+void Logger::Close()
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    if (logFp_) {
+        ::fclose(logFp_);
+        logFp_ = NULL;
+    }
+    logPathName_ = "";
+    rotateSize_ = 10*1024*1024;
+    currentSize_ = 0;
+    level_ = LogLevel::TRACE;
+    stdOut_ = false;
+    lastFlushSecs_ = 0;
+    forceFlushIntervel_ = 5;
+}
+
+void Logger::SetRotateSize(size_t size)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    rotateSize_ = size;
+}
+
+void Logger::SetLevel(LogLevel level)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    level_ = level;
+}
+
+void Logger::SetForceFlushIntervel(unsigned int seconds)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    forceFlushIntervel_ = seconds;
+}
+
+void Logger::SetStdout(bool on)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    stdOut_ = on;
+}
+
+void Logger::Log(LogLevel level, const char *format, ...)
+{
+    std::lock_guard<std::mutex> lock(mtx_);
+    if (!logFp_) {
+        fprintf(stderr,"Logger is not opened!!\n");
+        return;
+    }
+
+    if (level < level_) {
+        return;
+    }
+
+    va_list argptr;
+    va_start(argptr, format);
+    vsnprintf(logBuf_, 1024, format, argptr);
+    va_end(argptr);
+
+    Write(level);
+    Rotate();
+}
+
+const char* Logger::MapLevel(LogLevel level)
+{
+    if (level == LogLevel::TRACE) {
+        return "TRACE";
+    } else if (level == LogLevel::DEBUG) {
+        return "DEBUG";
+    } else if (level == LogLevel::INFO) {
+        return "INFO";
+    } else if (level == LogLevel::WARN) {
+        return "WARNING";
+    } else if (level == LogLevel::ERROR) {
+        return "ERROR";
+    } else {
+        return "Unknown Level";
+    }
+}
+
+void Logger::FormatTimeStamp()
+{
+    ::gettimeofday(&tv_, NULL);
+    int offset = ::strftime(timeBuf_, 64, "%Y%m%d %H:%M:%S.", ::localtime(&tv_.tv_sec));
+    snprintf(timeBuf_ + offset, 64 - offset, "%03d", (int)tv_.tv_usec/1000);
+}
+
+void Logger::Write(LogLevel level)
+{
+    FormatTimeStamp();
+    int size = fprintf(logFp_, "%s [%d] <%s> %s", timeBuf_, ThreadId(), MapLevel(level), logBuf_);
+
+    if (stdOut_) {
+        fprintf(stdout, "%s [%d] <%s> %s", timeBuf_, ThreadId(), MapLevel(level), logBuf_);
+    }
+
+    currentSize_ += size;
+    ::memset(timeBuf_, 0, 64);
+   ::memset(logBuf_, 0, 1024);
+ 
+    //force flush.
+    if (::difftime(tv_.tv_sec, lastFlushSecs_) > (double)forceFlushIntervel_) {
+        ::fflush(logFp_);
+        lastFlushSecs_ = tv_.tv_sec;
+    }
+}
+
+void Logger::Rotate()
+{
+    if (currentSize_ > rotateSize_) {
+        ::fclose(logFp_);
+
+        std::string oldLogFile = logPathName_;
+        oldLogFile.append(".old");
+        ::rename(logPathName_.c_str(), oldLogFile.c_str());
+
+        logFp_ = ::fopen(logPathName_.c_str(), "a+");
+        currentSize_ = 0;
+    }
+}
+
+}
---
+++ cxxlog.h
@@ -0,0 +1,110 @@
+#ifndef __CXXLOG_H__
+#define __CXXLOG_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <assert.h>
+#include <thread>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/syscall.h>
+#include <mutex> 
+
+
+namespace cxxlog {
+
+extern thread_local pid_t myThreadId_;
+
+enum class LogLevel {
+    TRACE = 0,
+    DEBUG,     
+    INFO,
+    WARN,
+    ERROR
+};
+
+// thread safe
+class Logger {
+
+public:
+    ~Logger();
+    //No Copying
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    void Open(const char *pathName);
+    void Close();
+    void SetRotateSize(size_t size);
+    void SetForceFlushIntervel(unsigned int seconds);
+    void SetLevel(LogLevel level);
+    void SetStdout(bool on);
+    void Log(LogLevel level, const char *format, ...);
+
+    static Logger* GetInstance() {
+        pthread_once(&pOnce_, &Logger::Init);
+        assert(instance_ != NULL);
+        return instance_;
+    }
+    
+    static void Init() {
+        instance_ = new Logger();
+    }
+
+    pid_t ThreadId() { 
+        if (myThreadId_ == 0) {
+            myThreadId_ = Gettid();
+        }
+        return myThreadId_; 
+    }
+   
+private:
+    Logger();
+    void FormatTimeStamp();
+    void Write(LogLevel level);
+    void Rotate();
+    const char* MapLevel(LogLevel level);
+
+    pid_t Gettid() {  
+        return syscall(SYS_gettid);  
+    }  
+
+private:
+    //singleton
+    static pthread_once_t pOnce_;
+    static Logger *instance_;
+
+    //log file
+    std::string logPathName_;
+    FILE *logFp_;
+    char logBuf_[1024];
+
+    //log option
+    size_t rotateSize_;
+    size_t currentSize_;
+    LogLevel level_;
+    bool stdOut_;
+
+    //log timestamp
+    struct timeval tv_;
+    time_t lastFlushSecs_;
+    unsigned int forceFlushIntervel_;
+    char timeBuf_[64];
+
+    std::mutex mtx_;
+};
+
+
+}
+
+#define CXXLOG_OPEN(pathName, stdOut) do { cxxlog::Logger::GetInstance()->Open(pathName); \
+    cxxlog::Logger::GetInstance()->SetStdout(stdOut); }while(0)
+#define CXXLOG_TRACE(format,...) do { cxxlog::Logger::GetInstance()->Log(cxxlog::LogLevel::TRACE, format, __VA_ARGS__); }while(0)
+#define CXXLOG_DEBUG(format,...) do { cxxlog::Logger::GetInstance()->Log(cxxlog::LogLevel::DEBUG, format, __VA_ARGS__); }while(0)
+#define CXXLOG_INFO(format,...) do { cxxlog::Logger::GetInstance()->Log(cxxlog::LogLevel::INFO, format, __VA_ARGS__); }while(0)
+#define CXXLOG_WARN(format,...) do { cxxlog::Logger::GetInstance()->Log(cxxlog::LogLevel::WARN, format, __VA_ARGS__); }while(0)
+#define CXXLOG_ERROR(format,...) do { cxxlog::Logger::GetInstance()->Log(cxxlog::LogLevel::ERROR, format, __VA_ARGS__); }while(0)
+
+
+#endif
+
